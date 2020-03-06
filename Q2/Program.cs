99cd605c97643afc0f9a9da5f350fff02a544e37
Nilashish Roy <nilashishroyjoy@gmail.com>
2020-03-06 06:00:00 +0600

    Added Q2 Program.cs

---
+++ Q2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Q2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      String contents = "";
+  int i,j;
+  	int[,] arr1 = new int[7,10];
+ 
+ 
+}
